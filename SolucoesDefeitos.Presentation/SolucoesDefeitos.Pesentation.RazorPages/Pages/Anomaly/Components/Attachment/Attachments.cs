7134b0d7374bb55e04e032f53f9dbf89dc5058eb
Tonimuiller Alves <tonymilleralves@gmail.com>
2023-06-09 20:23:19 -0300

    Attachments, started development, incomplete, not working

---
+++ SolucoesDefeitos.Presentation/SolucoesDefeitos.Pesentation.RazorPages/Pages/Anomaly/Components/Attachment/Attachments.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolucoesDefeitos.Pesentation.RazorPages.Pages.Anomaly.Components.Attachments;
+
+public class Attachments: ViewComponent
+{
+    public Attachments()
+    {
+        
+    }
+
+    public Task<IViewComponentResult> InvokeAsync(IEnumerable<Model.Attachment> attachments, CancellationToken cancellationToken)
+    {
+        attachments ??= new List<Model.Attachment>();
+        return Task.FromResult<IViewComponentResult>(View("~/Pages/Anomaly/Components/Attachment/Default.cshtml", attachments));
+    }
+}
