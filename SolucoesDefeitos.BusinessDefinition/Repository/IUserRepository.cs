79424dc36d7c56b04be5b528f3df1236bc8125c7
Tonimuiller Alves <toni@easycomtec.com>
2023-07-06 20:41:05 -0300

    Logout and user table sql definition

---
+++ SolucoesDefeitos.BusinessDefinition/Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using SolucoesDefeitos.Dto;
+using SolucoesDefeitos.Model;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SolucoesDefeitos.BusinessDefinition.Repository
+{
+    public interface IUserRepository: IRepository<User, int>
+    {
+        Task<ResponseDto<User>> GetByCredentialsAsync(string loginOrEmail, string password, CancellationToken cancellationToken);
+    }
+}
---
+++ SolucoesDefeitos.Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SolucoesDefeitos.Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public string Name { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
